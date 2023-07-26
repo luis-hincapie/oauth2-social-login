2abb8149fd9d7ac4b0ec5436e994ebaf9531dd6f
MOUSTAGHFIR <b.moustaghfir@brome.ma>
2023-07-26 16:21:16 +0100

    first commit

---
+++ src/main/java/com/badr/oauth2/social/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.badr.oauth2.social.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(cs -> cs.disable())
+                .authorizeHttpRequests(req -> req.anyRequest().authenticated())
+                .oauth2Login(withDefaults())
+                ;
+        return http.build();
+    }
+}
